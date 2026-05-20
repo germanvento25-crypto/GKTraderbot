3014e226e9e508162ca3841d5a7aca5414fe0975
germanvento25-crypto <germanvento25@gmail.com>
2026-05-20 13:08:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -182,5 +182,11 @@ def save():
     return redirect("/")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
